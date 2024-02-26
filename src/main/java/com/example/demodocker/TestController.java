bfc3ee624c2df0b0806812fe8f850947eb7c0c19
nkanton <nkanton@gmail.com>
2024-02-25 20:23:07 -0500

    simple h2 config

--- src/main/java/com/example/demodocker/TestController.java
+++ src/main/java/com/example/demodocker/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demodocker;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
 public class TestController {
+
+    @GetMapping
     public String test() {
         return "Hello World";
     }
